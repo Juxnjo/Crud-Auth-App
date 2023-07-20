c16a3df1cf2d456921488f37ce76778660bbf5d7
“Juan <ramirezjuanj0117@gmail.com>
2023-07-20 05:59:24 -0500

    password encriptada ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,27 @@
 import User from "../models/user.model.js"
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
     const {username, email, password} = req.body
     
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User({
         username,
         email,
-        password
+        password: passwordHash
     })
     console.log(newUser)
     const userSaved = await newUser.save()
-    res.json(userSaved)
+    res.json({
+        id: userSaved._id,
+        username: userSaved.username,
+        email: userSaved.email,
+        createdAt: userSaved.createdAt,
+        updatedAt: userSaved.updatedAt
+    })
           
     } catch (error) {
         console.log(error)
