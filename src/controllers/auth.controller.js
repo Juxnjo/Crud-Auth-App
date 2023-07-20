7465b6907596dff645c5c1b4d793736452cea6f6
“Juan <ramirezjuanj0117@gmail.com>
2023-07-20 07:20:49 -0500

    logout ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -60,3 +60,9 @@ export const login = async (req, res) => {
     }  
 }
 
+export const logout = (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
