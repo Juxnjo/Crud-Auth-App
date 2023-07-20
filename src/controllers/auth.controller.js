64a7afb75acfac56c6bee75217e498f596f5fbb3
“Juan <ramirezjuanj0117@gmail.com>
2023-07-20 06:38:57 -0500

    token ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcrypt from 'bcryptjs'
+import { createAccessToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
     const {username, email, password} = req.body
@@ -13,18 +14,23 @@ export const register = async (req, res) => {
         email,
         password: passwordHash
     })
+
     console.log(newUser)
+
     const userSaved = await newUser.save()
+    const token = await createAccessToken({id: userSaved._id})
+
+    res.cookie('token', token)
     res.json({
         id: userSaved._id,
         username: userSaved.username,
         email: userSaved.email,
         createdAt: userSaved.createdAt,
         updatedAt: userSaved.updatedAt
-    })
+    }) 
           
     } catch (error) {
-        console.log(error)
+        res.status(500).json({message: error.message})
     }  
 }
 
