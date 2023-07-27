2921a4616f82673d41c422c6c7c4f1d0f3226e62
“Juan <ramirezjuanj0117@gmail.com>
2023-07-26 23:55:03 -0500

    rutas crud

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,9 @@
+ export const getTasks = async (req, res) => {}
+ 
+ export const createTask = async (req, res) => {}
+
+ export const getTask = async (req, res) => {}
+
+ export const updateTask = async (req, res) => {}
+ 
+ export const deleteTask = async (req, res) => {}
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,12 +1,18 @@
 import {Router} from "express"
 import {authRequired} from "../middlewares/validateToken.js" 
+import {getTasks,getTask,createTask,updateTask,deleteTask} from '../controllers/tasks.controller.js'
 
 const router = Router()
 
-router.get('/tasks', authRequired, )
-router.get('/tasks/:id', authRequired, )
-router.post('/tasks', authRequired, )
-router.delete('/tasks/:id', authRequired, )
-router.put('/tasks/:id', authRequired, )
+router.get('/tasks', authRequired, getTasks )
+
+router.get('/tasks/:id', authRequired, getTask)
+
+router.post('/tasks', authRequired, createTask)
+
+router.delete('/tasks/:id', authRequired, deleteTask)
+
+router.put('/tasks/:id', authRequired, updateTask)
+
 
 export default router
