41e0184a2e0c1f106b7ea793e7b9e029164a25bc
“Juan <ramirezjuanj0117@gmail.com>
2023-08-02 16:00:37 -0500

    routes ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold underline">Probando Tailwind</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home page</h1>}/>
+        <Route path='/login' element={<h1>Login</h1>}/>
+        <Route path='/register' element={<h1>Register</h1>}/>
+        <Route path='/tasks' element={<h1>Tasks</h1>}/>
+        <Route path='/add-task' element={<h1>Add Tasks</h1>}/>
+        <Route path='/tasks/:id' element={<h1>Update task</h1>}/>
+        <Route path='/profile' element={<h1>Profile</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
