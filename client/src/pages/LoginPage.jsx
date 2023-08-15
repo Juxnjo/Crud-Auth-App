d19ab06fb1e9af97f45b245e5ea592e5aa4ad01e
“Juan <ramirezjuanj0117@gmail.com>
2023-08-15 12:42:49 -0500

    login OK

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,20 +1,32 @@
 import { useForm } from 'react-hook-form'
+import { useAuth } from '../context/AuthContext'
+import { Link } from "react-router-dom";
 
 export const LoginPage = () =>
 {
 
   const { register, handleSubmit, formState: { errors } } = useForm()
 
-  const onSubmit = handleSubmit( data =>
+  const { signin, errors: signinErrors } = useAuth()
+
+  const onSubmit = handleSubmit( ( data ) =>
   {
-    console.log( data )
+    signin( data )
   } )
-
-
 
   return (
     <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
       <div className='bg-purple-800 max-w-md w-full p-10 rounded-md '>
+        {
+          signinErrors.map( ( error, i ) => (
+            <div className='bg-red-500 p2 text-white text-center my-2' key={ i }>
+              { error }
+            </div>
+          ) )
+        }
+
+        <h1 className='text-2xl font-bold'>Login</h1>
+
         <form onSubmit={ onSubmit }>
 
           <input type="email" { ...register( 'email', { required: true } ) } className='w-full bg-purple-700 text-white px-4 py-2 rounded-md my-2' placeholder='Email' />
@@ -36,6 +48,10 @@ export const LoginPage = () =>
           <button type='submit' className='bg-purple-500 w-full hover:bg-purple-700 text-white font-semibold my-2 py-2 px-4 rounded-md'>Login</button>
 
         </form>
+        <p className='flex gap-x-2 justify-between'>
+          Dont have an account? <Link to="/register" className='text-sky-500'>Sign up</Link>
+        </p>
+
       </div>
 
     </div>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,8 @@
 import { useForm } from 'react-hook-form'
 import { useAuth } from '../context/AuthContext'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+
 
 export const RegisterPage = () =>
 {
@@ -23,14 +24,19 @@ export const RegisterPage = () =>
 
     return (
         <div className='flex h-[calc(100vh-100px)] items-center justify-center '>
-            {
-                registerErrors.map( ( error, i ) => (
-                    <div className='bg-red-500 p2 text-white' key={ i }>
-                        { error }
-                    </div>
-                ) )
-            }
+
             <div className='bg-purple-800 max-w-md w-full p-10 rounded-md '>
+
+                {
+                    registerErrors.map( ( error, i ) => (
+                        <div className='bg-red-500 p2 text-white text-center my-2' key={ i }>
+                            { error }
+                        </div>
+                    ) )
+                }
+
+                <h1 className='text-2xl font-bold'>Register</h1>
+
                 <form onSubmit={ onSubmit }>
 
                     <input type="text" { ...register( 'username', { required: true } ) } className='w-full bg-purple-700 text-white px-4 py-2 rounded-md my-2' placeholder='Username' />
@@ -60,6 +66,9 @@ export const RegisterPage = () =>
                     <button type='submit' className='bg-purple-500 w-full hover:bg-purple-700 text-white font-semibold my-2 py-2 px-4 rounded-md'>Register</button>
 
                 </form>
+                <p className='flex gap-x-2 justify-between'>
+                    Already have an account? <Link to="/login" className='text-sky-500'>Sign in</Link>
+                </p>
 
             </div>
 
