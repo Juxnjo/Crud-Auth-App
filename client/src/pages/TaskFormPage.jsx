547d47ecef25b731d3c265e351cc170630cdf559
“Juan <ramirezjuanj0117@gmail.com>
2023-08-16 14:56:51 -0500

    addtasks form

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -2,23 +2,30 @@ import { useForm } from "react-hook-form";
 
 export const TaskFormPage = () =>
 {
-  const {register, handleSubmit} = useForm()
+  const { register, handleSubmit } = useForm()
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
-  })
+  const onSubmit = handleSubmit( ( data ) =>
+  {
+    console.log( data )
+  } )
 
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input type="text" placeholder="Title" {...register('title')} autoFocus />
-        <textarea rows="10" placeholder="Description" {...register('description')}></textarea>
-        <button>
+    <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
+      <div className="bg-purple-800 max-w-md w-full p-10 rounded-md">
+        <h1 className='text-2xl font-bold'>Add Task</h1>
+        <form onSubmit={ onSubmit }>
+          <input type="text" placeholder="Title" { ...register( 'title' ) } className="w-full bg-purple-700 text-white px-4 py-2 my-2" autoFocus />
+
+          <textarea rows="10" placeholder="Description" { ...register( 'description' ) } className="w-full bg-purple-700 text-white px-4 py-2 my-2"></textarea>
+
+          <button className='bg-purple-500 w-full hover:bg-purple-700 text-white font-semibold my-2 py-2 px-4 rounded-md'>
           Save
         </button>
-      </form>
 
+        </form>
+
+      </div>
     </div>
   )
 }
