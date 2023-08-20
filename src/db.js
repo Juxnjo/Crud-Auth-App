6c249ff26b82355931f4a29cfa9b0bb20b19d4f6
Juan José <118330579+Juxnjo@users.noreply.github.com>
2023-08-20 13:34:32 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1/merndb");
+    await mongoose.connect("mongodb+srv://juxnjo:juxnjo.dev@mern-crud-auth-app.yk9fmbf.mongodb.net/?retryWrites=true&w=majority");
     console.log(">>> DB is connected");
   } catch (error) {
     console.log(error);
